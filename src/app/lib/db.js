a32243b23a92a4801a2240598af01d4fbb3a7387
Ahmedullah Farooqi <ahmedullahfarooqi8@gmail.com>
2024-07-15 21:16:06 +0500

    first commit

---
+++ src/app/lib/db.js
@@ -0,0 +1,2 @@
+// const {username,password} = process.env
+export const connectionStr="mongodb+srv://ahmedullahfarooqi34:ebad123@cluster0.il6c5wt.mongodb.net/restoDB?retryWrites=true&w=majority&appName=Cluster0"
