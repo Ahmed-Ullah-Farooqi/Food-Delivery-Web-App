77389da4861ebc3caabb5114988e4ef63802e0ad
Ahmedullah Farooqi <ahmedullahfarooqi8@gmail.com>
2024-07-15 21:44:16 +0500

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,5 @@
-// 'use client'
-// import { useState } from "react";
-// import RestaurantLogin from "./_components/restaurantLogin";
-// import RestaurantSignUp from "./_components/restaurantSignUp";
-// import RestaurantHeader from "./_components/RestaurantHeader";
-// import Footer from "./_components/Footer";
-// // import './style.css'
 
-// const Restaurant = () => {
-//     const [login, setLogin] = useState(true)
-//     return (
-//         <>
-//         <div className="container">
-//             <RestaurantHeader />
-//             <h1>Restaurant Login/Signup Page</h1>
-//             {
-//                 login ? <RestaurantLogin /> : <RestaurantSignUp />
-//             }
 
-//             <div>
-//                 <button className="button-link" onClick={() => setLogin(!login)}>
-//                     {login ? "Do not have account? SignUp" : "Already have Account? Login"}
-//                 </button>
-//             </div>
-//             </div>
-//             <Footer />
-//         </>
-//     )
-// }
-
-// export default Restaurant;
 
 'use client'
 import Image from "next/image";
